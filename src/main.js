e68ffcbdd1cc7a69be7b21289d05810c6603f141
LeeDahee23 <82389864+LeeDahee23@users.noreply.github.com>
2023-07-13 22:57:32 +0900

    JS-Arrowup

--- src/main.js
+++ src/main.js
@@ -17,3 +17,14 @@ const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+// 스크롤시 arrow-up 버튼 나타남
+const arrowUp = document.querySelector(".arrow-up");
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = 1;
+  } else {
+    arrowUp.style.opacity = 0;
+  }
+});
